2e2db158ea22f24599792c97e7a603ac6a3fd17b
vcordero07 <vcordero07@gmail.com>
2022-04-23 20:09:19 -0400

    sidebar is on the side

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -10,10 +10,14 @@ import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
 import Footer from "./Footer";
 const Layout = ({ children }) => {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <>
-      <Navbar />
-      {/* <Sidebar /> */}
+      <Navbar toggle={toggle}></Navbar>
+      <Sidebar isOpen={isOpen} toggle={toggle} />
       <main>{children}</main>
       {/* <Footer></Footer> */}
     </>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,12 +1,20 @@
-import React from 'react'
-import Links from '../constants/links'
-import Categories from '../components/Categories'
-import { IoMdClose } from 'react-icons/io'
+import React from "react";
+import Links from "../constants/links";
+import Categories from "../components/Categories";
+import { IoMdClose } from "react-icons/io";
 
-const Sidebar = () => {
+const Sidebar = ({ isOpen, toggle }) => {
   return (
-    <h4>sidebar component</h4>
-  )
-}
+    <aside className={`sidebar ${isOpen ? "showSidebar" : ""}`}>
+      <button className="close-btn" onClick={toggle}>
+        <IoMdClose />
+      </button>
 
-export default Sidebar
+      <div className="sidebar-container">
+        <Links styleClass="sidebar-links" />
+      </div>
+    </aside>
+  );
+};
+
+export default Sidebar;
