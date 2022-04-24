7d626d71dc9f166efb60f1fc7b2d9b3b871d6ac3
vcordero07 <vcordero07@gmail.com>
2022-04-24 18:07:08 -0400

    newsletter with netlify form set

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -1,9 +1,46 @@
-import React from 'react'
-import Layout from '../components/Layout'
+import React from "react";
+import Layout from "../components/Layout";
 const NewsLetter = () => {
   return (
-    <h4>newsletter</h4>
-  )
-}
+    <Layout>
+      <section className="newsletter-page">
+        <div className="page-center">
+          <h2>Get all the latest stories to your inbox</h2>
+          <h4>I write to my friends every few weeks</h4>
+          <form
+            action=""
+            className="contact-form"
+            name="testing-contact"
+            method="post"
+            netlify-honeypot="bot-field"
+            data-netlify="true"
+            action="/success"
+          >
+            <input type="hidden" name="bot-field" />
+            <input type="hidden" name="form-name" value="testing-contact" />
 
-export default NewsLetter
+            <input
+              type="text"
+              name="name"
+              placeholder="Your name"
+              className="form-control"
+            />
+
+            <input
+              type="email"
+              name="email"
+              placeholder="Your email"
+              className="form-control"
+            />
+
+            <button type="submit" className="btn form-control submit-btn">
+              subscribe
+            </button>
+          </form>
+        </div>
+      </section>
+    </Layout>
+  );
+};
+
+export default NewsLetter;
